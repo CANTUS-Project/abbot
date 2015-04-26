f6c050b70b79488978927db79bd04f4b97f7a4f6
Christopher Antila <christopher@antila.ca>
2015-04-25 23:09:48 -0400

    New: replace ChantIndexer with ComplexIndexer (for Cantusid, Source, Indexer too)
    It means the URL-specifying bit is a little long, but I've now consolidated all
    the resource types to two RequestHandler classes. Lots of work left to do, but
    I'm pleasantly surprised how easy this basic stuff is!

--- abbott/__main__.py
+++ abbott/__main__.py
@@ -22,6 +22,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #--------------------------------------------------------------------------------------------------
+'''
+'''
 
 # NOTE: this is just a sketch for now!
 
@@ -64,7 +66,7 @@ class TaxonomyHandler(web.RequestHandler):
         self.type_name = type_name
         self.type_name_plural = type_name_plural
         if additional_fields:
-            self.return_fields.extend(additional_fields)
+            self.returned_fields.extend(additional_fields)
 
     def format_record(self, record):
         '''
@@ -87,7 +89,7 @@ class TaxonomyHandler(web.RequestHandler):
         return post
 
     @gen.coroutine
-    def get(self, resource_id=None):
+    def basic_get(self, resource_id=None):
         '''
         '''
         if not resource_id:
@@ -110,133 +112,160 @@ class TaxonomyHandler(web.RequestHandler):
         self.set_header('Server', 'Abbott/{}'.format(ABBOTT_VERSION))
         self.add_header('X-Cantus-Version', 'Cantus/{}'.format(CANTUS_API_VERSION))
 
-        self.write(post)
-
-
-#class CantusidHandler(TaxonomyHandler):
-    ## TODO: write a superclass for this that will also do Chant and Indexer
-    #RETURNED_FIELDS = ['id', 'incipit', 'full_text', 'member_id']
-
-    #@gen.coroutine
-    #def get(self, cantusid_id):
-        #self.write((yield self.super_get(cantusid_id, 'cantusid', 'cantusids')))
-
-
-class ChantHandler(web.RequestHandler):
-    '''
-    '''
-    # List of all fields we should try to provide:
-    # incipit, source, marginalia, folio, sequence, office, genre, position, cantus_id, feast,
-    # feast_desc, mode, differentia, finalis, full_text, full_text_manuscript, full_text_simssa,
-    # volpiano, notes, cao_concordances, siglum, proofreader, cantus_id, melody_id
-
-    @staticmethod
-    @gen.coroutine
-    def format_chant(chant):
-        '''
-        Given a dict with a Solr response for a Chant record, convert the fields ending with "_id"
-        into the appropriate "name" or "title" of the corresponding resource.
-        '''
-        # NOTE: 'siglum' is a taxonomy term in Source but text field in Chant
-        DIRECT_INCLUDE = ['incipit', 'folio', 'position', 'sequence', 'mode', 'id', 'cantus_id',
-                          'full_text_simssa', 'full_text_manuscript', 'volpiano', 'notes',
-                          'cao_concordances', 'melody_id', 'marginalia', 'differentia', 'finalis',
-                          'siglum']
-        LOOKUP = {'feast_id': ('feast', 'feast'),
-                  'genre_id': ('genre', 'genre'),
-                  'office_id': ('office', 'office'),
-                  }  # value[0] is the "q_type" to lookup; value[1] is what it's called in "formatted"
-
-        formatted = {}
-
-        for key in iter(chant):
-            if key in DIRECT_INCLUDE:
-                formatted[key] = chant[key]
-            elif key in LOOKUP:
-                resp = yield ask_solr_by_id(LOOKUP[key][0], chant[key])
-                if len(resp) > 0:
-                    formatted[LOOKUP[key][1]] = resp[0]['name']
-            elif key == 'source_id':
-                resp = yield ask_solr_by_id('source', chant[key])
-                if len(resp) > 0:
-                    formatted['source'] = resp[0]['title']
-
-        # fetch extra information
-        if 'feast_id' in chant:  # for "feast_desc"
-            #results = yield ask_solr_by_id('feast', chant['feast_id'])
-            #for feast in results:
-                #formatted['feast_desc'] = feast['description']
-            resp = yield ask_solr_by_id('feast', chant['feast_id'])
-            if len(resp) > 0:
-                formatted['fest_desc'] = resp[0]['description']
-
-        if 'cantus_id' in chant:  # for "full_text"
-            resp = yield ask_solr_by_id('cantusid', chant['cantus_id'])
-            if len(resp) > 0 and 'full_text' in resp[0]:
-                formatted['full_text'] = resp[0]['full_text']
-
-        # TODO: implement the proofreader parts; they seem always empty in Drupal...
-
-        return formatted
+        return post
 
     @gen.coroutine
-    def get(self, chant_id=None):
-        # adjust for missing or malformed chant_id
-        if chant_id is None:
-            chant_id = '*'
-        elif chant_id.endswith('/') and len(chant_id) > 1:
-            chant_id = chant_id[:-1]
+    def get(self, resource_id=None):
+        '''
+        '''
+        self.write((yield self.basic_get(resource_id)))
 
-        # query Solr and format our response body
-        resp = yield ask_solr_by_id('chant', chant_id)
-        if len(resp) > 0:
-            post = ''
-            for chant in resp:
-                post += str((yield self.format_chant(chant))) + '\n\n'
-        else:
-            post = '[]\n\n'
 
-        # prepare the rest of the response
+class ComplexHandler(TaxonomyHandler):
+    '''
+    For the "Complex" resource types: cantusid, chant, indexer, and source.
+    '''
+
+    LOOKUP = {'feast_id': ('feast', 'name', 'feast'),
+              'genre_id': ('genre', 'description', 'genre'),
+              'office_id': ('office', 'name', 'office'),
+              'source_id': ('source', 'title', 'source'),
+              'provenance_id': ('provenance', 'name', 'provenance'),
+              'century_id': ('century', 'name', 'century'),
+              'notation_style_id': ('notation', 'name', 'notation_style'),
+              'segment_id': ('segment', 'name', 'segment'),
+              'source_status_id': ('source_status', 'name', 'source_status'),
+              'indexers': ('indexer', 'display_name', 'indexers'),
+              'editors': ('indexer', 'display_name', 'editors'),
+              'proofreaders': ('indexer', 'display_name', 'proofreaders'),
+             }
+    '''
+    For those fields that must be cross-referenced with the "name" field of a taxonomy resource.
+
+    - item 0: the "type" to lookup
+    - item 1: the field to replace with
+    - item 2: field name to use in output
+
+    Example: ``'genre_id': ('genre', 'description', 'genre')``.
+
+    Replaces the "genre_id" field with the "description" field of a "genre" record, stored in the
+    "genre" member on output.
+    '''
+    # TODO: item 1 will have to be configurable at runtime, because I'm sure some people would
+    #       rather read "A" rather than "Antiphon," for example
+
+    @gen.coroutine
+    def get(self, resource_id=None):
+        '''
+        '''
+        results = yield self.basic_get(resource_id)
+        post = {}
+
+        for record in iter(results):
+            if 'resources' == record:
+                post[record] = results[record]
+                continue
+
+            post[record] = {}
+            for field in iter(results[record]):
+                if field in ComplexHandler.LOOKUP:
+                    # TODO: refactor this to reduce duplication
+                    if isinstance(results[record][field], (list, tuple)):
+                        for value in results[record][field]:
+                            post[record][ComplexHandler.LOOKUP[field][2]] = []
+                            resp = yield ask_solr_by_id(ComplexHandler.LOOKUP[field][0], value)
+                            if len(resp) > 0:
+                                try:
+                                    post[record][ComplexHandler.LOOKUP[field][2]].append(resp[0][ComplexHandler.LOOKUP[field][1]])
+                                except KeyError:
+                                    # usually if the desired "field" isn't in the record we found
+                                    post[record][ComplexHandler.LOOKUP[field][2]].append(str(resp[0]))
+                        if 0 == len(post[record][ComplexHandler.LOOKUP[field][2]]):
+                            # if none of the things in the list ended up being found, we'll clear this out
+                            del post[record][ComplexHandler.LOOKUP[field][2]]
+                    else:
+                        resp = yield ask_solr_by_id(ComplexHandler.LOOKUP[field][0], results[record][field])
+                        if len(resp) > 0:
+                            post[record][ComplexHandler.LOOKUP[field][2]] = resp[0][ComplexHandler.LOOKUP[field][1]]
+                elif field in self.returned_fields:
+                    # NB: this generic branch must come after the LOOKUP branch. If it's before,
+                    #     LOOKUP fields will be matched in self.returned_fields and they won't be
+                    #     looked up.
+                    post[record][field] = results[record][field]
+
+            # if it's a Chant and doesn't have a "full_text" entry, we can get it from the cantusid
+            if 'full_text' in self.returned_fields and 'full_text' not in post[record] and 'cantus_id' in post[record]:
+                resp = yield ask_solr_by_id('cantusid', post[record]['cantus_id'])
+                if len(resp) > 0 and 'full_text' in resp[0]:
+                    post[record]['full_text'] = resp[0]['full_text']
+
+        # TODO: in chant, how to get feast and feast_desc from the feast_id?
+        # TODO: in source, how to get provenance and provenance_detail?
+        # TODO: in source, how to get source_status and source_status_desc?
+
         self.write(post)
 
 
 class RootHandler(web.RequestHandler):
+    '''
+    '''
+
     def get(self):
-        post = ('Abbott\n======\n' +
-                #'-> browse_cantusids: {}\n'.format(self.reverse_url('browse_s', 'id')) +
-                '-> browse_centuries: {}\n'.format(self.reverse_url('browse_centuries', 'id')) +
-                '-> browse_chants: {}\n'.format(self.reverse_url('browse_chants', 'id')) +
-                '-> browse_feasts: {}\n'.format(self.reverse_url('browse_feasts', 'id')) +
-                '-> browse_genres: {}\n'.format(self.reverse_url('browse_genres', 'id')) +
-                #'-> browse_indexers: {}\n'.format(self.reverse_url('browse_s', 'id')) +
-                '-> browse_notations: {}\n'.format(self.reverse_url('browse_notations', 'id')) +
-                '-> browse_offices: {}\n'.format(self.reverse_url('browse_offices', 'id')) +
-                '-> browse_portfolia: {}\n'.format(self.reverse_url('browse_portfolia', 'id')) +
-                '-> browse_provenances: {}\n'.format(self.reverse_url('browse_provenances', 'id')) +
-                '-> browse_sigla: {}\n'.format(self.reverse_url('browse_sigla', 'id')) +
-                '-> browse_segments: {}\n'.format(self.reverse_url('browse_segments', 'id')) +
-                #'-> browse_sources: {}\n'.format(self.reverse_url('browse_s', 'id')) +
-                '-> browse_source_statii: {}\n'.format(self.reverse_url('browse_source_statii', 'id')) +
-                '\n')
-        self.write(post)
+        '''
+        '''
+
+        all_plural_resources = [
+            'cantusids',
+            'centuries',
+            'chants',
+            'feasts',
+            'genres',
+            'indexers',
+            'notations',
+            'offices',
+            'portfolia',
+            'provenances',
+            'sigla',
+            'segments',
+            'sources',
+            'source_statii'
+            ]
+        post = {'browse_{}'.format(term): self.reverse_url('browse_{}'.format(term), 'id')
+                for term in all_plural_resources}
+        self.write({'resources': post})
 
 
 def make_app():
     '''
     NOTE: these URLs require a terminating /
+    TODO: make them not
     '''
+
     return web.Application([
         web.url(r'/', RootHandler),
-        # TODO: we need to get rid of these question marks because they mess up the "resources" URLs
-        #web.URLSpec(r'/cantusids/(.*/)?', CantusidHandler, name='browse_cantusids'),
+        web.URLSpec(r'/cantusids/(.*/)?', handler=ComplexHandler, name='browse_cantusids',
+                    kwargs={'type_name': 'cantusid', 'type_name_plural': 'cantusids',
+                            'additional_fields': ['incipit', 'full_text', 'genre_id']}),
         web.URLSpec(r'/centuries/(.*/)?', handler=TaxonomyHandler, name='browse_centuries',
                     kwargs={'type_name': 'century', 'type_name_plural': 'centuries'}),
-        web.URLSpec(r'/chants/(.*/)?', handler=ChantHandler, name='browse_chants'),
+        web.URLSpec(r'/chants/(.*/)?', handler=ComplexHandler, name='browse_chants',
+                    kwargs={'type_name': 'chant', 'type_name_plural': 'chants',
+                            'additional_fields': {'incipit', 'folio', 'position', 'sequence', 'mode',
+                                                  'id', 'cantus_id', 'full_text_simssa', 'full_text',
+                                                  'full_text_manuscript', 'volpiano', 'notes',
+                                                  'cao_concordances', 'melody_id', 'marginalia',
+                                                  'differentia', 'finalis', 'siglum', 'feast_id',
+                                                  'genre_id', 'office_id', 'source_id'}}),
         web.URLSpec(r'/feasts/(.*/)?', handler=TaxonomyHandler, name='browse_feasts',
-                    kwargs={'type_name': 'feast', 'type_name_plural': 'feasts'}),
+                    kwargs={'type_name': 'feast', 'type_name_plural': 'feasts',
+                            'additional_fields': ['date', 'feast_code']}),
         web.URLSpec(r'/genres/(.*/)?', handler=TaxonomyHandler, name='browse_genres',
-                    kwargs={'type_name': 'genre', 'type_name_plural': 'genres'}),
-        #web.URLSpec(r'/indexers/(.*/)?', IndexerHandler, name='browse_indexers'),
+                    kwargs={'type_name': 'genre', 'type_name_plural': 'genres',
+                            'additional_fields': ['mass_or_office']}),
+        web.URLSpec(r'/indexers/(.*/)?', handler=ComplexHandler, name='browse_indexers',
+                    kwargs={'type_name': 'indexer', 'type_name_plural': 'indexers',
+                            'additional_fields': ['display_name', 'given_name', 'family_name',
+                                                  'institution',' city', 'country']}),
         web.URLSpec(r'/notations/(.*/)?', handler=TaxonomyHandler, name='browse_notations',
                     kwargs={'type_name': 'notation', 'type_name_plural': 'notations'}),
         web.URLSpec(r'/offices/(.*/)?', handler=TaxonomyHandler, name='browse_offices',
@@ -249,13 +278,23 @@ def make_app():
                     kwargs={'type_name': 'siglum', 'type_name_plural': 'sigla'}),
         web.URLSpec(r'/segments/(.*/)?', handler=TaxonomyHandler, name='browse_segments',
                     kwargs={'type_name': 'segment', 'type_name_plural': 'segments'}),
-        #web.URLSpec(r'/sources/(.*/)?', SourceHandler, name='browse_sources'),
+        web.URLSpec(r'/sources/(.*/)?', handler=ComplexHandler, name='browse_sources',
+                    kwargs={'type_name': 'source', 'type_name_plural': 'sources',
+                            'additional_fields': ['title', 'rism', 'siglum', 'provenance_id',
+                                                  'date', 'century_id', 'notation_style_id',
+                                                  'segment_id', 'source_status_id', 'summary',
+                                                  'liturgical_occasions', 'description',
+                                                  'indexing_notes', 'indexing_date', 'indexers',
+                                                  'editors', 'proofreaders']}),
         web.URLSpec(r'/statii/(.*/)?', handler=TaxonomyHandler, name='browse_source_statii',
                     kwargs={'type_name': 'source_status', 'type_name_plural': 'source_statii'}),
         ])
 
 
 def main():
+    '''
+    '''
+
     app = make_app()
     app.listen(8888)
     ioloop.IOLoop.current().start()
