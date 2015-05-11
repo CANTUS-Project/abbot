9deea9acd77919315a17c37c5d327d0fa5c68c4b
Christopher Antila <christopher@antila.ca>
2015-05-10 22:31:35 -0400

    Tests: Add TestCase for SimpleHandler
    It's fully unit tested!

--- abbott/test_main.py
+++ abbott/test_main.py
@@ -36,6 +36,9 @@ import pysolrtornado
 from abbott import __main__ as main
 
 
+APPLICATION = web.Application(main.HANDLERS)
+
+
 def make_future(with_this):
     '''
     Creates a new :class:`Future` with the function's argument as the result.
@@ -43,6 +46,15 @@ def make_future(with_this):
     val = concurrent.Future()
     val.set_result(with_this)
     return val
+
+
+class TestHandler(testing.AsyncHTTPTestCase):
+    "Base class for classes that test a ___Handler."
+    def get_app(self):
+        return APPLICATION
+
+    def get_http_port(self):
+        return options.port
 
 
 class TestAbbott(unittest.TestCase):
@@ -75,3 +87,121 @@ class TestAbbott(unittest.TestCase):
         actual = yield main.ask_solr_by_id('genre', '162')
         self.assertEqual(expected, actual)
         main.SOLR.search.assert_called_once_with('+type:genre +id:162')
+
+
+class TestSimpleHandler(TestHandler):
+    '''
+    Tests for the SimpleHandler.
+    '''
+
+    def setUp(self):
+        "Make a SimpleHandler instance for testing."
+        super(TestSimpleHandler, self).setUp()
+        request = httpclient.HTTPRequest(url='/zool/', method='GET')
+        request.connection = mock.Mock()  # required for Tornado magic things
+        self.handler = main.SimpleHandler(self.get_app(), request, type_name='century')
+
+    def test_initialize_1(self):
+        "initialize() works with no extra fields"
+        self.assertEqual('century', self.handler.type_name)
+        self.assertEqual('centuries', self.handler.type_name_plural)
+        self.assertEqual(3, len(self.handler.returned_fields))
+
+    def test_initialize_2(self):
+        "initialize() works with extra fields"
+        request = httpclient.HTTPRequest(url='/zool/', method='GET')
+        request.connection = mock.Mock()  # required for Tornado magic things
+        actual = main.SimpleHandler(self.get_app(), request, type_name='genre', additional_fields=['mass_or_office'])
+        self.assertEqual('genre', actual.type_name)
+        self.assertEqual('genres', actual.type_name_plural)
+        self.assertEqual(4, len(actual.returned_fields))
+        self.assertTrue('mass_or_office' in actual.returned_fields)
+
+    def test_format_record_1(self):
+        "basic test"
+        input_record = {key: str(i) for i, key in enumerate(self.handler.returned_fields)}
+        input_record['false advertising'] = 'not allowed'  # this key obviously should never appear "for real"
+        expected = {key: str(i) for i, key in enumerate(self.handler.returned_fields)}
+        actual = self.handler.format_record(input_record)
+        self.assertEqual(expected, actual)
+
+    def test_make_resource_url_1(self):
+        "with no resource_type specified"
+        expected = '/centuries/420/'
+        actual = self.handler.make_resource_url('420')
+        self.assertEqual(expected, actual)
+
+    def test_make_resource_url_2(self):
+        "with singuler resource_type"
+        expected = '/chants/69/'
+        actual = self.handler.make_resource_url('69', 'chant')
+        self.assertEqual(expected, actual)
+
+    def test_make_resource_url_3(self):
+        "with plural resource_type"
+        expected = '/sources/3.14159/'
+        actual = self.handler.make_resource_url('3.14159', 'sources')
+        self.assertEqual(expected, actual)
+
+    @mock.patch('abbott.__main__.ask_solr_by_id')
+    @testing.gen_test
+    def test_basic_get_unit_1(self, mock_ask_solr):
+        "with no resource_id and Solr response has three things"
+        resource_id = None
+        mock_solr_response = [{'id': '1'}, {'id': '2'}, {'id': '3'}]
+        expected = {'1': {'id': '1'}, '2': {'id': '2'}, '3': {'id': '3'},
+                    'resources': {'1': {'self': '/centuries/1/'},
+                                  '2': {'self': '/centuries/2/'},
+                                  '3': {'self': '/centuries/3/'}}}
+        mock_ask_solr.return_value = make_future(mock_solr_response)
+        self.handler.set_header = mock.Mock()
+        self.handler.add_header = mock.Mock()
+
+        actual = yield self.handler.basic_get(resource_id)
+
+        mock_ask_solr.assert_called_once_with(self.handler.type_name, '*')
+        self.assertEqual(expected, actual)
+        self.handler.set_header.assert_called_once_with('Server',
+                                                        'Abbott/{}'.format(main.ABBOTT_VERSION))
+        self.handler.add_header.assert_called_once_with('X-Cantus-Version',
+                                                        'Cantus/{}'.format(main.CANTUS_API_VERSION))
+
+    @mock.patch('abbott.__main__.ask_solr_by_id')
+    @testing.gen_test
+    def test_basic_get_unit_2(self, mock_ask_solr):
+        "with resource_id ending in '/' Solr response is empty"
+        resource_id = '123/'
+        mock_solr_response = []
+        expected = {'resources': {}}
+        mock_ask_solr.return_value = make_future(mock_solr_response)
+        self.handler.set_header = mock.Mock()
+        self.handler.add_header = mock.Mock()
+
+        actual = yield self.handler.basic_get(resource_id)
+
+        mock_ask_solr.assert_called_once_with(self.handler.type_name, '123')
+        self.assertEqual(expected, actual)
+        self.handler.set_header.assert_called_once_with('Server',
+                                                        'Abbott/{}'.format(main.ABBOTT_VERSION))
+        self.handler.add_header.assert_called_once_with('X-Cantus-Version',
+                                                        'Cantus/{}'.format(main.CANTUS_API_VERSION))
+
+    @mock.patch('abbott.__main__.ask_solr_by_id')
+    @testing.gen_test
+    def test_basic_get_unit_3(self, mock_ask_solr):
+        "with resource_id not ending with '/' and Solr response has one thing"
+        resource_id = '888'  # such good luck
+        mock_solr_response = [{'id': '888'}]
+        expected = {'888': {'id': '888'}, 'resources': {'888': {'self': '/centuries/888/'}}}
+        mock_ask_solr.return_value = make_future(mock_solr_response)
+        self.handler.set_header = mock.Mock()
+        self.handler.add_header = mock.Mock()
+
+        actual = yield self.handler.basic_get(resource_id)
+
+        mock_ask_solr.assert_called_once_with(self.handler.type_name, '888')
+        self.assertEqual(expected, actual)
+        self.handler.set_header.assert_called_once_with('Server',
+                                                        'Abbott/{}'.format(main.ABBOTT_VERSION))
+        self.handler.add_header.assert_called_once_with('X-Cantus-Version',
+                                                        'Cantus/{}'.format(main.CANTUS_API_VERSION))
