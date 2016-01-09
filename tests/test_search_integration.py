f76f51a60b113ea816a13fbc9aaac297b653ae5a
Christopher Antila <christopher@antila.ca>
2016-01-09 16:03:59 -0500

    New: test_search_integration.py
    Yes, it's empty. The issue to write these tests is #82.
    
    For #81

---
+++ tests/test_search_integration.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Program Name:           abbot
+# Program Description:    HTTP Server for the CANTUS Database
+#
+# Filename:               tests/test_search_integration.py
+# Purpose:                Integration tests for SEARCH requests in SimpleHandler and ComplexHandler.
+#
+# Copyright (C) 2015, 2016 Christopher Antila
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------
+'''
+Integration tests for SEARCH requests in SimpleHandler and ComplexHandler.
+'''
+# NOTE: so long as no call is made into the "pysolrtornado" library, which happens when functions
+#       "in front of" pysolrtornado are replaced by mocks, the test classes needn't use tornado's
+#       asynchronous TestCase classes.
+
+# pylint: disable=protected-access
+# That's an important part of testing! For me, at least.
