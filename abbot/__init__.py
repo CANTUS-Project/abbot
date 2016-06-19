f1bbc2ea2ccfa99b54263a08e71356d9c2cb8ecf
Christopher Antila <christopher@antila.ca>
2016-06-19 18:16:15 -0400

    Fix abbot.__all__

--- abbot/__init__.py
+++ abbot/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['complex_handler', 'handlers', 'simple_handler', 'systemd_http_server', 'util']
+__all__ = ['complex_handler', 'handlers', 'simple_handler', 'util']
 __version__ = '0.6.12'
 __cantus_version__ = '0.2.3+'
 
